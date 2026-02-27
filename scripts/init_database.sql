13e5198b12b56f98dd1c1dddd91be3db92dae1b7
francescocolonello6 <francesco.colonello6@gmail.com>
2026-02-27 21:25:29 +0100

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,3 +1,8 @@
+'
+SCOPO: Serve per creare il DATABASE  e i relativi schemi
+
+ATTENZIONE: Usare questo script cancellerà e ricreerà il Database, cancellando tutti i dati. 
+Prima di utilizzarlo essere sicuri di avere backup'
 --Create Database 'Data Warehouse'
 	
 	USE master;
