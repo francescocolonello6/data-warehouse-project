60b66d7bd0f01958107299bfc8fb02277a57ca3f
francescocolonello6 <francesco.colonello6@gmail.com>
2026-03-01 19:46:51 +0100

    Update Cleaning_Silver.sql

--- scripts/Cleaning_Silver.sql
+++ scripts/Cleaning_Silver.sql
@@ -36,3 +36,34 @@ WHERE cst_id IS NOT NULL
 )T where flag_last = 1
 
 select * from silver.crm_cust_info
+
+
+INSERT INTO silver.crm_prd_info (
+prd_id,
+cat_id,
+prd_key,
+prd_nm,
+prd_cost,
+prd_line,
+prd_start_id,
+prd_end_dt
+)
+
+	SELECT 
+	prd_id,
+	REPLACE (SUBSTRING(prd_key, 1, 5), '-','_') as cat_id,
+	SUBSTRING (prd_key, 7, len(prd_key)) as prd_key,
+	prd_nm,
+	ISNULL(prd_cost,0) AS prd_cost,
+	CASE WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
+		 WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
+		 WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
+		 WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
+	ELSE 'n/a'
+	END AS prd_line,
+	CAST (prd_start_id as DATE) as prd_start_dt,
+	CAST (LEAD(prd_start_id) OVER (partition by prd_key order by prd_start_id)-1 as date) as prd_end_dt
+	FROM bronze.crm_prd_info
+
+select *
+from silver.crm_prd_info
