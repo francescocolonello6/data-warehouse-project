4a75fcb1f49ebeb56047a45209a7d224df9fe85c
francescocolonello6 <francesco.colonello6@gmail.com>
2026-03-08 20:47:38 +0100

    Create PRODUCT_view_Gold.sql

---
+++ scripts/Gold/PRODUCT_view_Gold.sql
@@ -0,0 +1,23 @@
+
+
+
+
+CREATE VIEW gold.dim_products AS
+SELECT
+ROW_NUMBER() OVER (ORDER BY pn.prd_start_id, pn.prd_key) as product_key,
+pn.prd_id as product_id,
+pn.prd_key as product_number,
+pn.prd_nm as product_name,
+pn.cat_id as category_id,
+pc.cat as category,
+pc.subcat as subcategory,
+pc.maintenance,
+pn.prd_cost as cost,
+pn.prd_line as product_line,
+pn.prd_start_id as stard_date
+
+FROM [silver].[crm_prd_info] pn
+LEFT JOIN [silver].[erp_px_cat_g1v2] pc
+ON		pn.cat_id = pc.id
+
+WHERE prd_end_dt IS NULL-- FILTER OUT HISTORICAL DATA -- 
