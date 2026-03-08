2691fa2078fb016103c959724704945ab1262af5
francescocolonello6 <francesco.colonello6@gmail.com>
2026-03-08 20:59:09 +0100

    Create Sales_View_Gold.sql

---
+++ scripts/Gold/Sales_View_Gold.sql
@@ -0,0 +1,20 @@
+
+
+
+CREATE VIEW gold.fact_sales AS
+SELECT
+sd.sls_ord_num as order_number,
+pr.product_key,
+cu.customer_key,
+sd.sls_order_dt as order_date,
+sd.sls_ship_dt as shipping_date,
+sd.sls_due_dt as due_date,
+sd.sls_sales as sales_amount,
+sd.sls_quantity as sales_quantity,
+sd.sls_price as sales_price
+
+FROM silver.crm_sales_details sd
+LEFT JOIN gold.dim_products pr
+ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customer cu
+ON sd.sls_cust_id = cu.customer_id
